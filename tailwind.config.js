da029fcc13fbc709b048a42bdc1bb93c2bd2d5c2
Arif <arifoyong@gmail.com>
2021-01-24 23:44:30 +0800

    Sidebar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'gray-900-spotify': '#121212',
+        'gray-800-spotify': '#181818',
+        'gray-700-spotify': '#282828',
+      },
+      lineHeight: {
+        'extra-loose': '2.5'
+      }
+    },
   },
   variants: {
     extend: {},
